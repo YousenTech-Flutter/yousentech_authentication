e0390185daf485fe2b9c5c8621e05a85c24da33e
mohmmedsaleh <mohmmed123232@gmail.com>
2025-01-23 12:50:01 +0300

    upate

--- lib/yousentech_authentication.dart
+++ lib/yousentech_authentication.dart
@@ -1,5 +1,8 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+library yousentech_authentication;
+
+export 'authentication/presentation/views/employees_list.dart';
