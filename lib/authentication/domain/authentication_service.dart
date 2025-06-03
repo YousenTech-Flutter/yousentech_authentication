ec4f2ccd82356c363b5f87844c34fa8453aafe91
mohmmedsaleh <120678169+mohmmedsaleh@users.noreply.github.com>
2025-06-03 15:50:12 +0300

    Update authentication_service.dart

--- lib/authentication/domain/authentication_service.dart
+++ lib/authentication/domain/authentication_service.dart
@@ -31,7 +31,7 @@ class AuthenticationService implements AuthenticationRepository {
         // 'pos_config_ids',
         // 'allowed_to_exceed_item_stock_quantity',
         'prevent_selling_with_negative_quantity',
-        'maximum_increase_allowed_unit_price',
+        // 'maximum_increase_allowed_unit_price',
         'maximum_decrease_allowed_unit_price',
         'edit_invoice_and_process_it_on_closing',
         'show_pos_app_settings',
